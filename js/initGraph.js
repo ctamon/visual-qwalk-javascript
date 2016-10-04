303184f2a8388519ead04d12b43bccdb2d562299
Nick <Nick@128.153.185.197>
2016-10-04 12:13:28 -0400

    Edited the add functions inside initGraph.js.

--- js/initGraph.js
+++ js/initGraph.js
@@ -10,82 +10,67 @@ function initGraph()
 	//Nodes
 
 
-	cy.add({
+	cy.add([
+            {group: "nodes",
+		data:{id: 'node1'},
+                position: { x: 100, y: 100}
+            }   
+        ]);
 
-		data:{
+	cy.add([
+            {group: "nodes",
+		data:{id: 'node2'},
+                position: { x: 200, y:100}
+	}
+    ]);
 
-			id: 'node1'
-
-		}
-
-	});
-
-	cy.add({
-
-		data:{
-
-			id: 'node2'
-
-		}
-
-	});
-
-	cy.add({
-
-		data:{
-
-			id: 'node3'
-
-		}
-
-	});
+	cy.add([
+            {group: "nodes",
+		data:{id: 'node3'},
+                position: { x: 300, y:100}
+	}
+    ]);
 
 	cy.getElementById('node1').data('isStart',true);
 
 
 	//Edges
 
-	cy.add({
-
+	cy.add([
+            {group: "edges",
 		data:{
-
 			id: 'edge12',
 			source: 'node1',
 			target: 'node2'
+                    }
+            }
+        ]);
 
-		}
 
-	});
-
-
-	cy.add({
-
+	cy.add([
+            {group: "edges",
 		data:{
-
 			id: 'edge23',
 			source: 'node2',
 			target: 'node3'
+                    }
+            }
+        ]);
 
-		}
-
-	});
-
-	cy.add({
-
+	cy.add([
+            {group: "edges",
 		data:{
-
 			id: 'edge31',
 			source: 'node3',
 			target: 'node1'
-
-		}
-
-	});
+                    }
+            }
+        ]);
 
 
 	//For now, we will lay it out in a circle
-	cy.layout({
-		name: 'circle'
-	});
+	//cy.layout({
+	//	name: 'circle'
+	//});
 
 }
