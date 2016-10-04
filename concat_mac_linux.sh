02df4f4a30ea6dce34a6713a0119506c1925c309
tuckergs <tuckergs@clarkson.edu>
2016-10-03 20:58:37 -0400

    Numeric js now usable, potential bug for library loading fixed
    I modified the concat batch files so they recognize Numeric.js. Also, these batch files don't recognize the other math libraries anymore: we don't need them. Also, in the index.html, I made it so the library include is first. This is to avoid potential issues that come from including code, dependent on a library, before said library

--- concat_mac_linux.sh
+++ concat_mac_linux.sh
@@ -4,5 +4,7 @@ rm lib_combined.js
 cat ./js/*.js >> combined.js
 
 cat ./lib/cytoscape.js >> lib_combined.js
-cat ./lib/complex.js >> lib_combined.js
-cat ./lib/math.js >> lib_combined.js
+cat ./lib/Numeric.js >> lib_combined.js
+
+# cat ./lib/complex.js >> lib_combined.js
+#cat ./lib/math.js >> lib_combined.js
