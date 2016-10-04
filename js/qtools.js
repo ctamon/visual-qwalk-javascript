0046bfdd3a80e83a95bb9c1dac2986400c7aa5da
Andres Colon <rivasm@clarkson.edu>
2016-10-04 12:28:25 -0400

    Implement qwalk and specdecomp in qtools.js
    The numeric.eig function in Numeric.js is unable to compute the
    eigenvectors for the adjacency matrix
    [[0 1 0]
     [1 0 1]
     [0 1 0]]
    because the solution fails to converge, even if the maximum number of
    iterations or the tolerance is increased. Therefore, this commit adds
    the compRealEigSys.js script, which computes the eigenvectors of the
    adjacency matrix.
    
    This commit also adds implementations of the specdecomp and qwalk
    functions. Now we can compute the quantum walk. Note that the
    implementations are untested.

--- js/qtools.js
+++ js/qtools.js
@@ -17,20 +17,18 @@ var qtools = {};
 // returns (Matrix): the resolved matrix
 //
 qtools.qwalk = function(A, t) {
-	var B = speedcomp(A);
+	var B = specdecomp(A);
 	var eigenvalues = B[0];
 	var eigenprojectors = B[1];
 
-	var size = math.size(A);
-	var num_rows = size[0];
-	var num_cols = size[1];
+	var size = math.size(A)[0];
 
-	var U = math.zeros(num_rows, num_cold)
+	var U = math.zeros(size, size);
 
 	var negImg = new Complex(0, -1); // -1j
 
 	for (var i = 0; i < eigenvalues.length; i++) {
-		U += math.exp(negImg * new Complex(t * eigenvalues[i])) * eigenprojectors[i];
+		U += math.exp(new Complex(0, -t) * eigenvalues[i]) * eigenprojectors[i];
 	}
 
 	return U;
@@ -40,13 +38,50 @@ qtools.qwalk = function(A, t) {
 // Computes the spectral decomposition of a matrix A
 //
 // params:
-//	A: input matrix
+//	A: square matrix
 //
 // returns: list of eigenvalues of A and a list of
 // 	corresponding eigenprojectors
 //
-qtools.speedcomp = function(A) {
+qtools.specdecomp = function(A) {
+	var N = math.size(A)[0];
+	var B = math.zeros(N, N)
+	var wr = math.zeros(N);
+	var wi = math.zeros(N);
+	var oPar = new Object();
+	oPar.outEr = 0;  // error code
 
+	calcEigSysReal(N, A, B, wr, wi, oPar);
+
+	/* At this point, the eigenvalues and eigenvectors of A have been computed.
+	 * The real components of the eigenvalues are in wr, the imaginary components
+	 * are in wi, the associated eigenvectors are in the B matrix, and oPar.outEr
+	 * contains the error code. */
+
+	var eigenvalue_list = new Array(N);
+	for (var i = 0; i < N; i++) {eigenvalue_list[i] = new Complex(wr[i], wi[i])}
+
+	B = numeric.T.transjugate(B);
+	var eigenvalues = new Array(N);
+	var eigenprojectors = new Array(N);
+	var count = 0;
+	for (var i = 0; i < N; i++) {
+		var found = false;
+		for (var j = 0; j < count; j++) {
+			if (abs(eigenvalue_list[i] - eigenvalues[j]) < 0.0001) {
+				var v = numeric.T.transjugate(B[i]);
+				eigenprojectors[j] += (v * numeric.T.transjugate(v));
+				found = true;
+			}
+		}
+		if (!found) {
+			eigenvalues[count] = eigenvalue_list[i];
+			var v = numeric.T.transjugate(eigenmatrix[i]);
+			eigenprojectors[count] = (v * numeric.T.transjugate(v));
+			count += 1;
+		}
+	}
+	return [eigenvalues, eigenprojectors];
 }
 
 
@@ -60,8 +95,13 @@ qtools.speedcomp = function(A) {
 // return:
 //	Matrix 2-norm of the Z - identity
 //
-qtools.testBias = function(A, eigenprojectors) {
-
+qtools.testBasis = function(A, eigenprojectors) {
+	var Z = math.zeros(size, size);
+	var size = math.size(A)[0];
+	for (var i = 0; i < eigenprojectors.length; i++) {
+		Z += eigenprojectors[i];
+	}
+	return math.norm(Z - math.eye(size), 2);
 }
 
 // Verifier: Checks if A = sum_{i} Eigenvalue[i] * EigenProjector[i]
@@ -75,5 +115,10 @@ qtools.testBias = function(A, eigenprojectors) {
 //	Matrix 2-norm of Z-A
 //
 qtools.testDecomp = function(A, eigenvalues, eigenprojectors) {
-
+	var size = math.size(A)[0];
+	var Z = math.zeros(size, size)
+	for (var i = 0; i < eigenvalues.length; i++) {
+		Z += eigenvalues[i] * eigenprojectors[i];
+	}
+	return math.norm(Z - A, 2);
 }
