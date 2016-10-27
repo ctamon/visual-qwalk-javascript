820af481943bdd47922d1e47a03f9597beff564b
Dan Hecker <heckerdj@Dans-MacBook-Pro-2.local>
2016-10-27 18:24:15 -0400

    UI Style changes. Added Play button switching to Stop Button

--- js/GUI.js
+++ js/GUI.js
@@ -5,3 +5,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+function play() {
+	var element = document.getElementById("PlayButton");
+	if (element.alt == "Play") {
+		element.alt = "Stop";
+		element.src = "StopButton.png";
+    	//elmt.innerHTML = "Stop";
+		//elmt.style.backgroundColor = 'red';
+}	else {
+		element.alt = "Play";
+		element.src = "PlayButton.png";
+    //elmt.innerHTML = "Play";
+	//elmt.style.backgroundColor = 'green';
+}
+
+}
