cea6b0dbb7de6774f819e58ccc3dda0528ba3560
Andres Colon <rivasm@clarkson.edu>
2016-11-06 13:17:32 -0500

    Restore valToColor function

--- js/qwalk.js
+++ js/qwalk.js
@@ -8,7 +8,7 @@ qwalk.startNode = 'n0'
 qwalk.timer = undefined
 
 function valToColor(value) {
-  if(value < 0 || value > 1) return '#ffffff'
+  if(value < 0 || value > 1) return '#000000'
   var c = Math.floor(value * 255)
   if(c < 16) return '#0' + c.toString(16) + '0000'
   else return '#' + c.toString(16) + '0000'
